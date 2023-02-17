1a6f499d46b8f32eaa0dbb7575aecf976338b1fe
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-17 07:02:38 -0800

    postive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,8 @@
 /**
  * main - Entry Point
  *
- * return: Always 0(success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -15,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive\n", n);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative", n)
+		printf("%d is negative\n", n);
 	}
 
 	return (0);

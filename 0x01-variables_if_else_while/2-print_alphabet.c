7f093635b53d667d5b119871f4b15bcb2c70b96c
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-17 23:36:42 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		printf("%c\n", ch);
+		ch++;
+	}
+	return (0);
+}
+

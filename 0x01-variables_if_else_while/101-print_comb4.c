b8a252c783aad6c887514c593b11b4b9cf6e8cf9
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-19 09:57:48 -0800

    comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 48; y <= 57; y++)
+		{
+			for (z = 48; z <= 57; z++)
+			{
+				if (z > y && y > x)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56 || z != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

3fe6a2c1a1a3c2eb47cd49b311553bf9730aa999
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-23 10:16:50 -0800

    most_num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print most numbers
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}

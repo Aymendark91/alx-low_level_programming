212e788ce18e74738c989e8190c6d628434be838
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-23 00:45:48 -0800

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks the alphabet
+ * @c: checks character
+ *
+ * Return: 1 if true else o
+ */
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 122);
+}

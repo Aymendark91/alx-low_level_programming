35041f77121f137907c60522d46dc6a94d87eee2
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-25 08:51:03 -0800

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,10 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar(z + '0');
 			}
-			else if (z > 10)
+			else if (z >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');

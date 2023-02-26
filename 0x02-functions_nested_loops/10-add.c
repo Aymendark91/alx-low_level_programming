23524d578db05fda3abbb206498e8743db0b2698
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-26 04:32:23 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add numbers
+ * @a: checks character
+ * @b: checks character
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}

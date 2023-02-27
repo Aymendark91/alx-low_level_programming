6c4a2085f6ed5d5e62c15cadac25d5a388a67d81
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-27 00:12:06 -0800

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print numbers to 98
+ * @n: checks character
+ */
+void print_to_98(int n)
+{
+	int x, y;
+
+	if (n <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (n == 98)
+			{
+				printf("%d\n", a);
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (n == 98)
+			{
+				printf("%d\n", b);
+			}
+		}
+	}
+}

e6a70e61d59cd9621b7af2d843bc37a58eb1b9b4
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-27 01:06:11 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,13 @@
 /**
  * _putchar - print putchar
  *
- *
+ * @c: check character
  * Return: Always 0 (Success)
  */
-int _putchar(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
+	c = '_putchar'
 	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
 	_putchar('\n');
 	return (0);
 }

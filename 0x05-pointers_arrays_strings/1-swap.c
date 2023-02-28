f9e01dc574f4a6f0f2fb1e60e31cbd4c36c608b1
Aymendark91 <mohammedaymen670@gmail.com>
2023-02-28 09:13:47 -0800

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,9 +4,13 @@
  * swap_int - swap two pointer
  * @a: checks character
  * @b: checks character
+ * 
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }

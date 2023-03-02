0adbd814e0e15f5267fe84917997a017fdb049a3
Aymendark91 <mohammedaymen670@gmail.com>
2023-03-02 10:19:01 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - strcmp
+ * @s1: s1
+ * @s2: s2
+ *
+ * Return: return their difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}

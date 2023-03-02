cd5b730fea8a7bf6f4bb27ab5f7805393446d52b
Aymendark91 <mohammedaymen670@gmail.com>
2023-03-02 08:22:09 -0800

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,7 @@
  */
 void rev_string(char *s)
 {
+	char tmp;
 	int i, len, len1;
 
 	len = 0;
@@ -20,7 +21,7 @@ void rev_string(char *s)
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len];
-		s[len--] = temp;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }

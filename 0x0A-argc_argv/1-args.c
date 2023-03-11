6e6e6be3490590c321a5cc3aed6af34c2bc808fb
Aymendark91 <mohammedaymen670@gmail.com>
2023-03-11 09:28:55 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - main
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%s\n", argc - 1);
+
+	return (0);
+}
